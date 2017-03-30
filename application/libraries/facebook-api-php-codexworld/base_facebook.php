a1b4ac103119e1dc10ee4fb2a5d8f25ce385f554
onlystar <onlystar1991@hotmail.com>
2017-03-30 02:44:28 +0200

    facebook login controller name changed

--- application/libraries/facebook-api-php-codexworld/base_facebook.php
+++ application/libraries/facebook-api-php-codexworld/base_facebook.php
@@ -135,6 +135,8 @@ abstract class BaseFacebook
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_TIMEOUT        => 60,
     CURLOPT_USERAGENT      => 'facebook-php-3.2',
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => 2
   );
 
   /**
